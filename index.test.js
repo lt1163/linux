f16526f893f6a56d66910058fbaaebadf43bb326
Dweb <kws@gmail.com>
2024-12-10 23:23:04 +0900

    my first ci test

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const { sum, abs } = require("./index.js");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test("adds 100 -50 to equal 50", () => {
+  expect(abs(100, -50)).toBe(150);
+});
+test("adds 100 -50 to equal 50", () => {
+  expect(abs(-100, 50)).toBe(150);
+});
